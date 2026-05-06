73dbd5d386e079075099ed74b1d006da9877ed4c
DIBESH KUMAR PARIDA <dibeshkumarparida2610@gmail.com>
2026-05-06 18:52:53 +0530

    feat: add MyResult page with grouped quiz results UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes,Navigate} from 'react-router-dom';
 import Home from './pages/Home';
+import MyResultPage from './pages/MyResultPage';
+import { useAuth,Show } from '@clerk/react';
+
 
 const App = () => {
+  const { isLoaded } = useAuth();
+  if (!isLoaded) return null;
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/result" element={
+        <>
+          <Show when="signed-in">
+            <MyResultPage />
+          </Show>
+          <Show when="signed-out">
+            <Navigate to="/" />
+          </Show>
+        </>
+      } />
     </Routes>
 
   )
